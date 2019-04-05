73d2046801962a523c92cce6b3f0fec61f428367
MuraHika <kima.bright@mail.ru>
2019-04-05 23:22:19 +0400

    lab4

---
+++ AbstractSecurityShop/AbstractSecurityShopServiceDAL/Interface/IReptService.cs
@@ -0,0 +1,19 @@
+﻿using AbstractSecurityShopServiceDAL.BindingModel;
+using AbstractSecurityShopServiceDAL.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractSecurityShopServiceDAL.Interface
+{
+    public interface IReptService
+    {
+        void SaveTechnicsPrice(ReptBindingModel model);
+        List<StorageLoadViewModel> GetStorageLoad();
+        void SaveStorageLoad(ReptBindingModel model);
+        List<CustomerOrdersViewModel> GetCustomerOrders(ReptBindingModel model);
+        void SaveCustomerOrders(ReptBindingModel model);
+    }
+}
