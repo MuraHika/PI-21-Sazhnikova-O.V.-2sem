f19658612d86ba80ef1f4963da1a5cd1b684776d
MuraHika <kima.bright@mail.ru>
2019-06-15 09:15:05 +0400

    web1

---
+++ AbstractGiftShop/AbstractSecurityShopWebView/Controllers/CustomerController.cs
@@ -0,0 +1,68 @@
+﻿using AbstractSecurityShopServiceDAL.BindingModel;
+using AbstractSecurityShopServiceDAL.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AbstractSecurityShopWebView.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ICustomerService _service;
+
+        public CustomerController(ICustomerService service)
+        {
+            _service = service;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_service.GetList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreatePost()
+        {
+            _service.AddElement(new CustomerBindingModel
+            {
+                CustomerFIO = Request["CustomerFIO"]
+            });
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var viewModel = _service.GetElement(id);
+            var bindingModel = new CustomerBindingModel
+            {
+                Id = id,
+                CustomerFIO = viewModel.CustomerFIO
+            };
+            return View(bindingModel);
+        }
+
+        [HttpPost]
+        public ActionResult EditPost()
+        {
+            _service.UpdElement(new CustomerBindingModel
+            {
+                Id = int.Parse(Request["Id"]),
+                CustomerFIO = Request["CustomerFIO"]
+            });
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            _service.DelElement(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
